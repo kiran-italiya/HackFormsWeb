a82ea88634c1977dac4a6a74f27b7a5a844d813b
ksjadeja <kuldeepjadeja459@gmail.com>
2020-02-16 04:33:14 +0530

    initial commit

--- HackForms/views.py
+++ HackForms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request,'home.html')
+    # return HttpResponse("Hello, world. You're at the polls index.")
--- HackFormsWeb/settings.py
+++ HackFormsWeb/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'HackFormsWeb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,5 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+# TEMPLATE_DIRS = (os.path.join(BASE_DIR,  'templates'),)
