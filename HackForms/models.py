83b5c74526652ac80718502315dc3259909a8c82
Kiran1901 <kiran.italiya19@gmail.com>
2020-02-17 19:25:22 +0530

    working 2

--- HackForms/models.py
+++ HackForms/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from HackFormsWeb import settings
 
 
 class Project(models.Model):
     project_name = models.CharField(max_length=100, unique=True,primary_key=True)
-    empty_form = models.FilePathField(path='/media/')
-    zip_file = models.FilePathField(path='/media/')
-    data = models.FilePathField(path='/media/')
-    csv_file = models.FilePathField(path='/media/')
+    empty_form = models.FilePathField(path=settings.MEDIA_ROOT)
+    zip_file = models.FilePathField(path=settings.MEDIA_ROOT)
+    data = models.FilePathField(path=settings.MEDIA_ROOT)
+    csv_file = models.FilePathField(path=settings.MEDIA_ROOT)
     def __str__(self):
         return  self.project_name
