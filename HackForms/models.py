a25556a8b175eca68e5de1e457dd93c73c13a49b
ksjadeja <kuldeepjadeja459@gmail.com>
2020-02-17 10:43:41 +0530

    commit

--- HackForms/models.py
+++ HackForms/models.py
@@ -1,12 +1,5 @@
 from django.db import models
 
-# Create your models here.
-# class NewProjectForm(models.Model):
-#     def __init__(self,proj_name):
-#         self.project_name = proj_name
-#     project_name = models.CharField(max_length=50,unique=True)
-#     empty_form = models.FileField(upload_to=project_name)
-#     zip_file = models.FileField(upload_to=project_name)
 
 class Project(models.Model):
     project_name = models.CharField(max_length=100, unique=True,primary_key=True)
