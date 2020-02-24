b26d756339e981db65f058897086bb5bcc5f505b
Kiran1901 <kiran.italiya19@gmail.com>
2020-02-24 16:28:09 +0530

    git ignore adjusted

--- HackForms/models.py
+++ HackForms/models.py
@@ -4,9 +4,9 @@ from HackFormsWeb import settings
 
 class Project(models.Model):
     project_name = models.CharField(max_length=100, unique=True,primary_key=True)
-    empty_form = models.FilePathField(path=settings.MEDIA_ROOT)
-    zip_file = models.FilePathField(path=settings.MEDIA_ROOT)
-    data = models.FilePathField(path=settings.MEDIA_ROOT)
-    csv_file = models.FilePathField(path=settings.MEDIA_ROOT)
+    empty_form = models.FileField(path=settings.MEDIA_ROOT)
+    zip_file = models.FileField(path=settings.MEDIA_ROOT)
+    data = models.FileField(path=settings.MEDIA_ROOT)
+    csv_file = models.FileField(path=settings.MEDIA_ROOT)
     def __str__(self):
         return  self.project_name
