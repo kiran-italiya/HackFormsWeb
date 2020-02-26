859f0562ea433ed94327b446002268127158d768
Kiran1901 <kiran.italiya19@gmail.com>
2020-02-26 20:49:31 +0530

    Revert "functions added in main"
    This reverts commit bd18922077286c620eb6d4cbf26d017236316acf.

--- HackForms/Processing/hackForm.py
+++ HackForms/Processing/hackForm.py
@@ -341,40 +341,40 @@ def hackForm(csvfile):
     df = df.sort_values(by=['top']) #.reset_index(drop=True)
     df['group'] = df['group'].astype(object)
     # print("Original:\n ",df)
-
-
+    
+    
     min_field_height = df['height'].min()
     max_field_height = df['height'].max()
     avg_field_height = df['height'].mean()
 
 
 
-
+    
     element=-1
     parent_group=None
     # print(df.shape[0])
     UNREACHABLE= -1000
-
+    
     newDf=pd.DataFrame(columns=df.columns)
-
+    
     while(element < df.shape[0]-1):
         element+=1;labels=0;fields=0;checkboxes=0;radios=0
         in_strip_elements=0
         topy=0;bottomy=0
-
+    
         local_min_top = 0
         local_max_bottom = 0
-
+    
         # first_top=df.iloc[element].top
         # first_height=df.iloc[element].height
-
+    
         ERROR = (max_field_height - df.iloc[element].height )/2 #, 0.25*df.iloc[element].height)
         topy=df.iloc[element].top - ERROR
         bottomy=df.iloc[element].height+df.iloc[element].top + ERROR
-
+    
         curr_df = df[(df.top>=topy) & (df.top+df.height<=bottomy)].copy()
         curr_df=curr_df.sort_values(by='left') # .reset_index(drop=True)
-
+    
         # print("\nOld ERROR: ",ERROR)
         # #
         # for i in range(curr_df.shape[0]):
@@ -390,10 +390,10 @@ def hackForm(csvfile):
         #
         # curr_df = df[(df.top >= topy) & (df.top + df.height <= bottomy)].copy()
         # curr_df = curr_df.sort_values(by='left')  # .reset_index(drop=True)
-
+    
         # print("topy:",topy," bottomy:",bottomy)
         # print("\n curr_df::\n ",curr_df)
-
+    
         for i in range(curr_df.shape[0]):
             element+=1
             in_strip_elements+=1
@@ -406,25 +406,25 @@ def hackForm(csvfile):
             if curr_df.iloc[i].type == 'radio':
                 radios += 1
         element-=1
-
-
+    
+    
         if in_strip_elements==1:
             if curr_df.iloc[0].type=='label':
                 parent_group=curr_df.index[0]
                 # set parent_group to all elements in below strip
-
+    
             if curr_df.iloc[0].type=='field':
                 if parent_group is not None:
                     df.at[curr_df.index[0],'group']=parent_group
                 else:
                     print("parent_group is missing so assigning useless")
                     df.at[curr_df.index[0], 'group'] = UNREACHABLE
-
+    
             if curr_df.iloc[0].type == 'checkbox' or curr_df.iloc[0].type == 'radio':
                 print("single checkbox/radio is useless, so assigning useless")
                 df.at[curr_df.index[0], 'group'] = UNREACHABLE
-
-
+    
+    
         if labels>0 and fields>0:
             if checkboxes>0 or radios>0:
                 print("Form is inappropriate")
@@ -435,18 +435,18 @@ def hackForm(csvfile):
                 else:
                     parent_group = None
                     curr_df = assign_with_missing(curr_df, parent_group, df,labels,fields)
-
-
+    
+    
         if labels > 0 and checkboxes > 0:
             if fields>0 or radios>0:
                 print("Form is inappropriate")
             else:
                 curr_df = assign_from_last(curr_df, parent_group, df)
-
+    
         if(radios>0):
             if (labels>0 and labels==radios+1) or (labels==radios and parent_group is not None):    #perfect result
                 curr_df=assign_from_last(curr_df,parent_group,df)
-
+    
             else:
                 if labels==0 and parent_group is not None:
                     i = 1
@@ -459,15 +459,15 @@ def hackForm(csvfile):
                     for index, row in curr_df.loc[curr_df['type'] == 'radio'].iterrows():
                         df.at[index, 'group'] = [parent_group, i * -1]
                         i += 1
-
+    
                 else:
                     i = 1
                     for index,row in curr_df.loc[curr_df['type'] == 'radio'].iterrows():
                         df.at[index, 'group'] = ['useless', i * -1]
                         i += 1
-
-
-
+    
+    
+    
     # print('new DF:\n', df)   #df[df.type!='label'])
     df.to_csv('res.csv')
     return df
@@ -499,35 +499,6 @@ def create_dict_from_df(dfx):
                 mappingDict.update(tmpDict)
 # create_dict_from_df(df)
 # print(mappingDict)
-
-
-def data_dict(df, final_df):
-    dict = {}
-    tmp = []
-    for i, row in df[df.type == 'label'].iterrows():
-        tmp.append(row.value)
-    dict['labels'] = tmp
-
-    tmp = []
-    for i, row in df[df.type == 'field'].iterrows():
-        tmp.append([df[df.loc[row.group]].value, row.value])
-    dict['fields'] = tmp
-
-    tmp = []
-    for i, row in df[df.type == 'checkbox'].iterrows():
-        try:
-            tmp.append([df.loc[list(row.group)[0]].value, df.loc[list(row.group)[1]].value,
-                        final_df[df.loc[list(row.group)[1]].value].value])
-        except Exception as e:
-            print(e)
-    dict['checkboxes'] = tmp
-
-    tmp = []
-    for i, row in df[df.type == 'radio'].iterrows():
-        tmp.append([list(df[df.loc[row.group]].value)[0], final_df[list(df[df.loc[row.group]].value)[0]].value])
-    dict['radios'] = tmp
-
-    return dict
 
 
 def perform_OCR(df):
--- HackForms/Processing/main.py
+++ HackForms/Processing/main.py
@@ -1,193 +1,165 @@
 import detection 
 import hackForm
 import extraction
-import cv2,os
+import cv2
 import pytesseract
 import imutils,copy,csv
 import pandas as pd
 import numpy as np
+import math
 
-class ProcessForm:
+img = cv2.imread("kiran.jpg")
+width = 1000
+height = 40
+diff = 30
 
-	def __init__(self):
+datacsv='data.csv'
 
-		self.width= 1000
-		self.height = 40
-		self.diff = 30
+img = imutils.resize(img, width=width)
+h,w = img.shape[:2]
+img3 = img.copy()
+img1 = img.copy()
+img2 = img.copy()
+cv2.imshow("crop", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-		self.img = cv2.imread("kiran.jpg")
-		self.datacsv='data.csv'
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, threshold = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+gray = cv2.medianBlur(gray, 3)
+threshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 31,2)
+gray = cv2.erode(threshold, None)
+gray = cv2.dilate(gray, None)
 
-		self.self.start=0
+#"""Label Detection and Processing"""
 
-		self.database={}
+cnt = detection.contour(img1)
 
-	def process_empty_form(self,img):
+#    """RECTANGLE DETECTION"""
 
-		img = imutils.resize(img, width=self.width)
-		h,w = img.shape[:2]
-		img1 = img.copy()
-		cv2.imshow("crop", img)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
+rectangle_image,rec_coordinate=detection.detect_rectangles(img)
+df_box = detection.eliminate_duplicate_box(rec_coordinate,diff)
+cv2.imwrite("boxes.jpg",rectangle_image)
+#"""Line detection and processing"""    
+line = detection.linesp(img)
+df = detection.line_processing(line,diff, height)
+img2 = img.copy()
+for row in df.itertuples():
+	cv2.rectangle(img2, (row[1],row[2]-40),(row[3],row[4]),(0,0,255),1)
+cv2.imwrite("lines.jpg",img2)
+#"""Duplicate BOX/LINE ELIMINATION"""   
+df,df_box = detection.eliminate_duplicate_entry(df,df_box)
+#    """CHECKBOX DETECTION"""
+df_box, checkbox = detection.get_checkbox(df_box)
 
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		_, threshold = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-		gray = cv2.medianBlur(gray, 3)
-		threshold = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 31,2)
-		gray = cv2.erode(threshold, None)
-		gray = cv2.dilate(gray, None)
+#    """FORM BOX """
+start = df_box.iloc[0]
+print(start)
+#    """CONVERSION TO H,W FROM X2,Y2"""
+df['Y1'] = df['Y1'] - height
+df['Y2'] = df['Y2'] + 10
 
-		#"""Label Detection and Processing"""
+df_box = df_box.append(df)
+df_box = df_box.sort_values(by= ['Y1','X1']).reset_index(drop=True)
+df_box['Type'] = 'field'
+df_box['Value'] = np.nan
+df_box['Group'] = 0
+temp1 = copy.deepcopy(df_box['X1'])
+temp2 = copy.deepcopy(df_box['X2'])
+temp3 = copy.deepcopy(df_box['Y1'])
+temp4 = copy.deepcopy(df_box['Y2'])
 
-		cnt = detection.contour(img1)
+df_box['Y2'] = abs(temp2-temp1)
+df_box['X2'] = abs(temp4-temp3)
+df_box['Y1'] = df_box['Y1'] - 5
+df_box['X2'] = df_box['X2'] + 10
 
-		#    """RECTANGLE DETECTION"""
+#"""ADJUSTING COORDINATES TO BOUNDING BOX"""
+if (start[3]-start[1])>h/2:
+	print("VERY AUSPICIOUS CASE ENCOUNTERED")
+	df_box = df_box.drop(df_box.index[0])
+	df_box['Y1'] = df_box['Y1'] - start[1]
+	df_box['X1'] = df_box['X1'] - start[0]
 
-		rectangle_image,rec_coordinate=detection.detect_rectangles(img)
-		df_box = detection.eliminate_duplicate_box(rec_coordinate,self.diff)
-		cv2.imwrite("boxes.jpg",rectangle_image)
-		#"""Line detection and processing"""
-		line = detection.linesp(img)
-		df = detection.line_processing(line,self.diff, self.height)
-		img2 = img.copy()
-		for row in df.itertuples():
-			cv2.rectangle(img2, (row[1],row[2]-40),(row[3],row[4]),(0,0,255),1)
-		cv2.imwrite("lines.jpg",img2)
-		#"""Duplicate BOX/LINE ELIMINATION"""
-		df,df_box = detection.eliminate_duplicate_entry(df,df_box)
-		#    """CHECKBOX DETECTION"""
-		df_box, checkbox = detection.get_checkbox(df_box)
+field_box = df_box.values.tolist()
 
-		#    """FORM BOX """
-		self.start = df_box.iloc[0]
-		print(self.start)
-		#    """CONVERSION TO H,W FROM X2,Y2"""
-		df['Y1'] = df['Y1'] - self.height
-		df['Y2'] = df['Y2'] + 10
+circles = detection.findCircle(cnt, img1)
 
-		df_box = df_box.append(df)
-		df_box = df_box.sort_values(by= ['Y1','X1']).reset_index(drop=True)
-		df_box['Type'] = 'field'
-		df_box['Value'] = np.nan
-		df_box['Group'] = 0
-		temp1 = copy.deepcopy(df_box['X1'])
-		temp2 = copy.deepcopy(df_box['X2'])
-		temp3 = copy.deepcopy(df_box['Y1'])
-		temp4 = copy.deepcopy(df_box['Y2'])
+cv2.imwrite("img.png",img1)
+with open(datacsv, 'w', newline='') as file:
+	writer = csv.writer(file)
+	writer.writerow(["left", "top", "height", "width", "type", "value", "group"])
+	writer.writerows(field_box)
+	writer.writerows(circles)
+	writer.writerows(checkbox)
 
-		df_box['Y2'] = abs(temp2-temp1)
-		df_box['X2'] = abs(temp4-temp3)
-		df_box['Y1'] = df_box['Y1'] - 5
-		df_box['X2'] = df_box['X2'] + 10
-
-		#"""ADJUSTING COORDINATES TO BOUNDING BOX"""
-		if (self.start[3]-self.start[1])>h/2:
-			print("VERY AUSPICIOUS CASE ENCOUNTERED")
-			df_box = df_box.drop(df_box.index[0])
-			df_box['Y1'] = df_box['Y1'] - self.start[1]
-			df_box['X1'] = df_box['X1'] - self.start[0]
-
-		field_box = df_box.values.tolist()
-
-		circles = detection.findCircle(cnt, img1)
-
-		cv2.imwrite("img.png",img1)
-		with open(self.datacsv, 'w', newline='') as file:
-			writer = csv.writer(file)
-			writer.writerow(["left", "top", "height", "width", "type", "value", "group"])
-			writer.writerows(field_box)
-			writer.writerows(circles)
-			writer.writerows(checkbox)
-
-		pad = 10
-		df = pd.read_csv(self.datacsv)
-		x,y = threshold[0:self.start[1]-30,:].shape
-		threshold[0:self.start[1]-30,:] = np.ones((x,y))*255
-		for row in df.itertuples():
-			x,y = threshold[row[2]-pad:row[2]+row[3]+pad,row[1]-pad:row[1]+pad+row[4]].shape
-			threshold[row[2]-pad:row[2]+row[3]+pad,row[1]-pad:row[1]+pad+row[4]] = np.ones((x,y))*255
+pad = 10
+df = pd.read_csv(datacsv)
+x,y = threshold[0:start[1]-30,:].shape
+threshold[0:start[1]-30,:] = np.ones((x,y))*255
+for row in df.itertuples():
+	x,y = threshold[row[2]-pad:row[2]+row[3]+pad,row[1]-pad:row[1]+pad+row[4]].shape
+	threshold[row[2]-pad:row[2]+row[3]+pad,row[1]-pad:row[1]+pad+row[4]] = np.ones((x,y))*255
 
 
 
 
-		cv2.imshow("img",threshold)
-		cv2.waitKey(0)
-		cv2.destroyAllWindows()
-		text = pytesseract.image_to_boxes(threshold, lang = 'eng',config= '--psm 4')
-		data = text.split('\n')
-		height= 25
-		df = detection.generate_label_box(data,height)
+cv2.imshow("img",threshold)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+text = pytesseract.image_to_boxes(threshold, lang = 'eng',config= '--psm 4')
+data = text.split('\n')
+height= 25
+df = detection.generate_label_box(data,height)
 
-		y,x = threshold.shape
-		df['Y1'] = y - 25 - df['Y1']
-		df['X1'] = df['X1'] - 5
-		df['X2'] = df['X2'] + 10
-		df['Y2'] = df['Y2'] + 10
-		if (self.start[3]-self.start[1])>h/2:
-			df['Y1'] = df['Y1'] - self.start[1]
-			df['X1'] = df['X1'] - self.start[0]
+y,x = threshold.shape
+df['Y1'] = y - 25 - df['Y1']
+df['X1'] = df['X1'] - 5
+df['X2'] = df['X2'] + 10
+df['Y2'] = df['Y2'] + 10
+if (start[3]-start[1])>h/2:
+	df['Y1'] = df['Y1'] - start[1]
+	df['X1'] = df['X1'] - start[0]
 
-		label_box = df.values.tolist()
+label_box = df.values.tolist()
 
-		with open(self.datacsv, 'a', newline='') as file:
-			writer = csv.writer(file)
-			writer.writerows(label_box)
+with open(datacsv, 'a', newline='') as file:
+	writer = csv.writer(file)
+	writer.writerows(label_box) 
 
-		df_box= pd.read_csv(self.datacsv,     encoding = "ISO-8859-1")
+df_box= pd.read_csv(datacsv,     encoding = "ISO-8859-1")
 
-		# cv2.imshow("Display", img1)
-		# cv2.waitKey(0)
-		# cv2.destroyAllWindows()
-		# cv2.imwrite("result.jpg", img1)
-
-
-		df = hackForm.hackForm(self.datacsv)
-		return df
+# cv2.imshow("Display", img1)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# cv2.imwrite("result.jpg", img1)
 
 
-	def process_filled_form(self, df, img):
-
-		#"""INSERT LOOP FOR PROCESSING IMAGES IN BULK"""
-		# img = cv2.imread("kiran2.png")
-		img = imutils.resize(img, width = 1000)
-		rectangle_image,rec_coordinate=detection.detect_rectangles(img)
-		df_box = detection.eliminate_duplicate_box(rec_coordinate,self.diff)
-		df_box = df_box.sort_values(by= ['Y1','X1']).reset_index(drop=True)
-		dst_img = df_box.iloc[0]
-		h_dst,w_dst = img.shape[:2]
-		if (self.start[3]-self.start[1])>h_dst/2 and (dst_img[3]-dst_img[1])>h_dst/2:
-			img = extraction.transformation(img,self.start,dst_img)
-			img = img[self.start[1]:self.start[3],self.start[0]:self.start[2]]
-			cv2.imshow("testcrop",img)
-			cv2.waitKey(0)
-			cv2.destroyAllWindows()
-		label = []
-		for _,row in df.iterrows():
-			if row['type'] == 'label' and row['group'] == 'NaN':
-				label.append(row['value'])
-		dummy = [np.nan]*len(label)
-		print(dummy)
-		df_final = pd.DataFrame([dummy],columns =label)
-		print(df_final)
-		df_final = extraction.radio_identification(img,df,df_final)
-		print(df_final)
-		df_final = extraction.checkbox_identification(img,df,df_final)
-		print(df_final)
-		df_final.to_csv('final.csv')
-
-		dict = hackForm.data_dict(df,df_final)
-		return dict
-
-
-	def processForm(self,img,path):
-
-		df = ProcessForm.process_empty_form(img=img)
-		for file in os.listdir(path):
-			if file.endswith(".jpg"):
-				tmp_df=df
-				dict = ProcessForm.process_filled_form(df=tmp_df, img=file)
-			self.database[file]=dict
-
-
-ProcessForm.processForm('k1.jpg',os.getcwd()+"/data")
+df = hackForm.hackForm(datacsv)
+#"""INSERT LOOP FOR PROCESSING IMAGES IN BULK"""
+img = cv2.imread("kiran2.png")
+img = imutils.resize(img, width = 1000)
+rectangle_image,rec_coordinate=detection.detect_rectangles(img)
+df_box = detection.eliminate_duplicate_box(rec_coordinate,diff)
+df_box = df_box.sort_values(by= ['Y1','X1']).reset_index(drop=True)
+dst_img = df_box.iloc[0]
+h_dst,w_dst = img.shape[:2]
+if (start[3]-start[1])>h/2 and (dst_img[3]-dst_img[1])>h_dst/2:
+	img = extraction.transformation(img,start,dst_img)
+	img = img[start[1]:start[3],start[0]:start[2]]
+	cv2.imshow("testcrop",img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
+label = []
+for _,row in df.iterrows():
+	if row['type'] == 'label' and row['group'] == 'NaN':
+		label.append(row['value'])
+dummy = [np.nan]*len(label)
+print(dummy)
+df_final = pd.DataFrame([dummy],columns =label)
+print(df_final)
+df_final = extraction.radio_identification(img,df,df_final)
+print(df_final)
+df_final = extraction.checkbox_identification(img,df,df_final)
+print(df_final)
+df_final.to_csv('final.csv')
